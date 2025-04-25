0e6bf6f341f98cc53b79285df058e0c3d8d26607
Bhavana-hub01 <gandambhavana43@gmail.com>
2025-04-25 13:15:25 +0530

    Add files via upload

---
+++ customer_data_analysis.sql
@@ -0,0 +1,45 @@
+-- Show all customer records
+SELECT * FROM customers;
+
+-- Show total number of customers
+SELECT COUNT(*) FROM customers;
+
+-- Show transactions for Chennai market (market code: 'Mark001')
+SELECT * FROM transactions 
+WHERE market_code = 'Mark001';
+
+-- Show distinct product codes that were sold in Chennai
+SELECT DISTINCT product_code FROM transactions 
+WHERE market_code = 'Mark001';
+
+-- Show transactions where currency is US dollars
+SELECT * FROM transactions 
+WHERE currency = 'USD';
+
+-- Show transactions in 2020 joined with the date table
+SELECT transactions.*, date.* 
+FROM transactions 
+INNER JOIN date ON transactions.order_date = date.date 
+WHERE date.year = 2020;
+
+-- Show total revenue in year 2020 (for INR or USD)
+SELECT SUM(transactions.sales_amount) AS total_revenue 
+FROM transactions 
+INNER JOIN date ON transactions.order_date = date.date 
+WHERE date.year = 2020 
+  AND (transactions.currency = 'INR' OR transactions.currency = 'USD');
+
+-- Show total revenue in January 2020 (for INR or USD)
+SELECT SUM(transactions.sales_amount) AS total_revenue 
+FROM transactions 
+INNER JOIN date ON transactions.order_date = date.date 
+WHERE date.year = 2020 
+  AND date.month_name = 'January' 
+  AND (transactions.currency = 'INR' OR transactions.currency = 'USD');
+
+-- Show total revenue in 2020 in Chennai market
+SELECT SUM(transactions.sales_amount) AS total_revenue 
+FROM transactions 
+INNER JOIN date ON transactions.order_date = date.date 
+WHERE date.year = 2020 
+  AND transactions.market_code = 'Mark001';
